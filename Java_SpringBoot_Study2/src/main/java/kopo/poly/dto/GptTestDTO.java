d68b02601054aa6ac085dad39a7da1b667313454
SaeByeol <toquf345@gmail.com>
2023-10-17 22:50:20 +0900

    GPT API 받아보기

--- Java_SpringBoot_Study2/src/main/java/kopo/poly/dto/GptTestDTO.java
+++ Java_SpringBoot_Study2/src/main/java/kopo/poly/dto/GptTestDTO.java
@@ -6,5 +6,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class GptTestDTO {
-    private String gptMessage;
+
+    //GPT에서 나에게 주는 메세지
+   String gptMessage;
+
+   //이용자가 GPT에게 주는 메세지
+   String userMessage;
 }
