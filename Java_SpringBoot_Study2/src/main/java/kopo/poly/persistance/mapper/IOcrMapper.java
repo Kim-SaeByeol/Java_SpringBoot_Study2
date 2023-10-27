ae0be34583a03aca51bf10a0d6ae121a994bdbd8
SaeByeol <toquf345@gmail.com>
2023-10-27 22:57:06 +0900

    23.10.27 과제

---
+++ Java_SpringBoot_Study2/src/main/java/kopo/poly/persistance/mapper/IOcrMapper.java
@@ -0,0 +1,14 @@
+package kopo.poly.persistance.mapper;
+
+import kopo.poly.dto.OcrDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IOcrMapper {
+    List<OcrDTO> getOcrList() throws Exception;
+
+    //게시판 글 등록
+    void insertOcrInfo(OcrDTO pDTO) throws Exception;
+}
